d8850812f007dac63bfa19310790ac2626b2e618
DanielBalda <38044851+DanielBalda@users.noreply.github.com>
2022-06-09 16:41:28 -0300

    Api y Regex para filtro de fechas

--- proyecto_final.py
+++ proyecto_final.py
@@ -2,29 +2,18 @@
 # no laborables (sábados y domingos).
 # Debe incluir la posibilidad de ingresar un rango de fechas
 
+from ast import pattern
+from tabnanny import check
 import tkinter
-from tkcalendar import DateEntry
-import requests
-from datetime import date
 import os
 import sys
-
+import requests
+import re
 
 class Interface():
 
-    def resource_path(self, relative_path):
-        try:
-            base_path = sys._MEIPASS
-        except Exception:
-            base_path = os.path.abspath(".")
-        return os.path.join(base_path, relative_path)
-
     def __init__(self):
-        url = 'http://nolaborables.com.ar/api/v2/feriados/2022'
-        response = requests.get(url)
-        if response.status_code == 200:
-            content = response.content
-            print(content)
+        #self.apiData()
         self.window = tkinter.Tk()
         self.window.config(background="#333333")
         self.window.title("Final Automatas y Gramaticas")
@@ -32,7 +21,6 @@ class Interface():
         # self.window.iconbitmap(self.resource_path('FOLDER/assets/icono.ico'))
         self.window.resizable(False, False)
         self.window.geometry("440x560")
-        dateTime = str(date.today()).split("-")
 
         dateTitle = tkinter.Label(self.window,
                                   text="Busqueda por Fecha:",
@@ -48,14 +36,13 @@ class Interface():
                                fg="#519ABA")
         title1.place(x=50, y=45)
 
-        cal = DateEntry(self.window,
-                        width=10,
-                        bg="darkblue",
-                        fg="white",
-                        day=int(dateTime[0]),
-                        month=int(dateTime[1]),
-                        year=int(dateTime[2]))
-        cal.place(x=110, y=45)
+        self.calFirst = tkinter.Entry(self.window,
+                                      background="#606060",
+                                      fg="#FFFFFF",
+                                      font="Helvetica 10 bold",
+                                      highlightthickness=1,
+                                      width=10)
+        self.calFirst.place(x=110, y=45)
 
         title2 = tkinter.Label(self.window,
                                text="Hasta:",
@@ -64,14 +51,13 @@ class Interface():
                                fg="#519ABA")
         title2.place(x=220, y=45)
 
-        cal = DateEntry(self.window,
-                        width=10,
-                        bg="darkblue",
-                        fg="white",
-                        day=int(dateTime[0]),
-                        month=int(dateTime[1]),
-                        year=int(dateTime[2]))
-        cal.place(x=275, y=45)
+        self.calLast = tkinter.Entry(self.window,
+                                     background="#606060",
+                                     fg="#FFFFFF",
+                                     font="Helvetica 10 bold",
+                                     highlightthickness=1,
+                                     width=10)
+        self.calLast.place(x=275, y=45)
 
         self.btnFilter = tkinter.Button(self.window,
                                         text=" BUSCAR ",
@@ -105,15 +91,53 @@ class Interface():
         scroll.config(command=self.textBox.yview)
         self.textBox.place(x=6, y=140)
 
-        textTmp = ("TEST")
-        self.textBox.delete(1.0, tkinter.END)
-        self.textBox.insert(tkinter.INSERT, textTmp, 'lightblue')
-
         # Crea y muestra la interfaz
         self.window.mainloop()
 
+    def resource_path(self, relative_path):
+        try:
+            base_path = sys._MEIPASS
+        except Exception:
+            base_path = os.path.abspath(".")
+        return os.path.join(base_path, relative_path)
+
+    def apiData(self):
+        self.fecha = []
+        for anio in range(5):
+            url = 'http://nolaborables.com.ar/api/v2/feriados/20'+str(anio+18)
+            response = requests.get(url)
+            if(response.status_code == 200):
+                for element in response.json():
+                    self.fecha.append(str(element['dia']) +
+                                      '/'+str(element['mes']) +
+                                      '/20'+str(anio+18))
+        return self.fecha
+
+    def checkInputs(self):
+        REpattern = r'([0-2][0-9]|3[0-1])(/|-)(0[1-9]|1[0-2])\2(\d{4})'
+        checkDate = 0
+        if re.match(REpattern, self.calFirst.get()) is None:
+            self.calFirst.config(highlightbackground="red",
+                                 highlightcolor="red")
+        else:
+            self.calFirst.config(highlightbackground="green",
+                                 highlightcolor="green")
+            checkDate += 1
+        if re.match(REpattern, self.calLast.get()) is None:
+            self.calLast.config(highlightbackground="red",
+                                highlightcolor="red")
+        else:
+            self.calLast.config(highlightbackground="green",
+                                highlightcolor="green")
+            checkDate += 1
+        if(checkDate == 2):
+            return True
+        else:
+            return False
+
     def options(self, option):
-        if(option == 1):
+        self.textBox.delete(1.0, tkinter.END)
+        if(option == 1 and self.checkInputs()):
             self.textBox.delete(1.0, tkinter.END)
             with open(self.resource_path('./data_red.txt'), 'r') as dataFile:
                 lines = dataFile.readlines()
@@ -121,16 +145,12 @@ class Interface():
                     if(index != 0):
                         line = line.split(";")
                         resultsBox = "ID: "+line[0] + \
-                                     " - Fecha: "+line[2] + \
+                                     " - Fecha: "+line[2].split(' ')[0] + \
                                      "\n"
-                        self.textBox.insert(tkinter.INSERT, resultsBox, 'lightblue')
-    def api():
-        url = 'http://nolaborables.com.ar/api/v2/feriados/2022'        ##api
-        response = requests.get(url)
-        if response.status_code == 200:
-            content = response.content
-            print(content)
+                        self.textBox.insert(tkinter.INSERT,
+                                            resultsBox,
+                                            'lightblue')
+
 
 if __name__ == "__main__":
     Interface()
-    
